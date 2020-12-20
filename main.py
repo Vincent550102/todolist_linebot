f617bf0f1550331d8d512ce30835540f95f79105
Vincent <5020559@gmail.com>
2020-12-20 22:47:07 +0800

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+# 載入需要的模組
+from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+line_bot_api = LineBotApi('10cfe0b9-052c-47ea-95d4-e99fa8761c99
+')
+handler = WebhookHandler('2bb08b3eefd3898727ae8ab11436a05f')
+
+# 接收 LINE 的資訊
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+if __name__ == "__main__":
+    app.run()
