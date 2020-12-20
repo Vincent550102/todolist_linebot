635e7d3777b2220866afb8dc26655569c6711816
Vincent <5020559@gmail.com>
2020-12-21 00:50:02 +0800

    v15

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import *
 
 import configparser
 
