8670de44d9603b26150185a2d88e8de648f29a00
Vincent <5020559@gmail.com>
2020-12-20 23:40:06 +0800

    v2

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-# 載入需要的模組
 from __future__ import unicode_literals
 import os
 from flask import Flask, request, abort
-
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 import configparser
 
 app = Flask(__name__)
 
+# LINE 聊天機器人的基本資料
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-# LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -40,7 +39,6 @@ def echo(event):
         event.reply_token,
         TextSendMessage(text=event.message.text)
     )
-
 
 if __name__ == "__main__":
     app.run()
