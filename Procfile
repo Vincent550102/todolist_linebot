7217ba1e2a4b4d782f4c1d2045a015f223994740
Vincent <5020559@gmail.com>
2020-12-20 23:37:32 +0800

    v1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main:app –preload
+web: gunicorn Python main:app –preload
--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 from __future__ import unicode_literals
 import os
 from flask import Flask, request, abort
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import configparser
 
 app = Flask(__name__)
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
 # LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('10cfe0b9-052c-47ea-95d4-e99fa8761c99')
-handler = WebhookHandler('2bb08b3eefd3898727ae8ab11436a05f')
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -25,6 +32,15 @@ def callback():
         abort(400)
 
     return 'OK'
+
+# 學你說話
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
 
 if __name__ == "__main__":
     app.run()
