2e9e6b7cc7bec687779973a62c4a735fc6d3ed9f
Vincent <5020559@gmail.com>
2020-12-24 17:01:13 +0800

    tmp50

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def echo(event):
         result = '_ToDoList_\n'
         db = json.load(open('DataBase.json', encoding='utf-8'))
         print(db)
-        for idx,item in enumerate(ToDoList):
+        for idx,item in enumerate(db[uid]):
             result += '{}. {}\n'.format(str(idx+1),item)
         print(db)
         line_bot_api.reply_message(
