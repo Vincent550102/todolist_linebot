23aa30f8e2c0d3e795dbc3246ac2f8eb20edc641
Vincent <5020559@gmail.com>
2020-12-24 23:00:41 +0800

    upd1

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print(body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
